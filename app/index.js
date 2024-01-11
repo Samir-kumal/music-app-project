6da9a77a4c9667b01ba9e684175544d6fabd0254
Samir-kumal <kumalsameer124@gmail.com>
2024-01-11 20:35:38 +0545

    refactor: clean code for index.js  and  add StatusLoading Component and Auth Context

--- app/index.js
+++ app/index.js
@@ -1,21 +1,10 @@
-import { View, Text } from 'react-native'
-import React,{useContext, useEffect, useState} from 'react'
-import { DataContext } from '../src/context/DataProvider'
+import React from 'react'
 import { Redirect } from 'expo-router'
-import Loading from '../src/components/Common/Loading'
 const index = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const {userInfo, isLoggedIn} = useContext(DataContext)
+
+  return <Redirect href={"(auth)/signin"} />
 
 
-  setTimeout(()=>{
-    setIsLoading(false)
-  },2000)
-  return (
-    <>
-   <Redirect href={"(auth)/onboard"} />
-    </>
-  )
 }
 
-export default index
+export default index;
